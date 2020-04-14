ec576be50654c362419027f49e2c6bf4dbf3d805
Bob Ross <pianodaemon@gmail.com>
2020-04-13 21:19:06 -0500

    bis code id at store procedure alter observations
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- DOS/migrations/stores.sql
+++ DOS/migrations/stores.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION public.alter_observation(_observation_id integer, _type_id integer, _code_id integer, _social_program_id integer, _audit_id integer, _fiscal_id integer, _title text, _amount_observed double precision, _amount_projected double precision, _amount_solved double precision, _amount_comments text) RETURNS record
+CREATE FUNCTION public.alter_observation(_observation_id integer, _type_id integer, _code_id integer, _bis_code_id integer, _social_program_id integer, _audit_id integer, _fiscal_id integer, _title text, _amount_observed double precision, _amount_projected double precision, _amount_solved double precision, _amount_comments text) RETURNS record
     LANGUAGE plpgsql
     AS $$
 
@@ -23,6 +23,7 @@ BEGIN
             INSERT INTO observations (
                 observation_type_id,
                 observation_code_id,
+                observation_bis_code_id,
                 social_program_id,
                 audit_id,
                 title,
@@ -32,6 +33,7 @@ BEGIN
             ) VALUES (
                 _type_id,
                 _code_id,
+                _bis_code_id,
                 _social_program_id,
                 _audit_id,
                 _title,
@@ -75,7 +77,7 @@ BEGIN
 
             UPDATE observations
             SET title  = _title, observation_type_id = _type_id, observation_code_id = _code_id,
-                social_program_id = _social_program_id,
+                observation_bis_code_id = _bis_code_id, social_program_id = _social_program_id,
                 audit_id = _audit_id, fiscal_id = _fiscal_id,
                 amount_observed = _amount_observed, touch_latter_time = current_moment
             WHERE id = _observation_id;

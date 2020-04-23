1cc44e298792aff2bb203fc24c74aced840adf81
userd <omontes.dev@gmail.com>
2020-04-23 11:12:50 -0500

    Added permissions support to User endpoints

--- DOS/soa/service/genl/endpoints/users.py
+++ DOS/soa/service/genl/endpoints/users.py
@@ -19,23 +19,34 @@ user_ns_captions = {
 
 ns = api.namespace("users", description="Available services for a user")
 
-user = api.model('User', {
+usr_fields = {
     'id': fields.Integer(description=user_ns_captions['id']),
     'username': fields.String(description=user_ns_captions['username'], required=True),
     'passwd': fields.String(description=user_ns_captions['passwd'], required=True),
     'orgchart_role_id': fields.Integer(description=user_ns_captions['orgchart_role_id'], required=True),
     'division_id': fields.Integer(description=user_ns_captions['division_id'], required=True),
     'disabled': fields.Boolean(description=user_ns_captions['disabled']),
-})
+}
+user = api.model('User', usr_fields)
 
-pair = api.model('id-title pair', {
+usr_fields_ext = dict(usr_fields)
+usr_fields_ext['access_vector'] = fields.List(fields.Integer(description='Authority id'), required=True)
+user_ext = api.model('Extended User', usr_fields_ext)
+
+pair = api.model('Id-Title pair', {
     'id': fields.Integer(description='An integer as entry identifier'),
-    'title': fields.String(description='Entry\'s title or description'),
+    'title': fields.String(description='Entry title'),
 })
 
-catalog = api.model('Users screen data (id-title pairs catalog for screen fields)', {
+pair2 = api.model('Id-Description pair', {
+    'id': fields.Integer(description='An integer as entry identifier'),
+    'description': fields.String(description='Entry description'),
+})
+
+catalog = api.model('Users screen data (catalog of Id-Title pairs for screen fields)', {
     'divisions': fields.List(fields.Nested(pair)),
     'orgchart_roles': fields.List(fields.Nested(pair)),
+    'authorities': fields.List(fields.Nested(pair2)),
 })
 
 @ns.route('/')
@@ -80,8 +91,8 @@ class UserList(Resource):
         return users_list, 200, {'X-SOA-Total-Items': total_items, 'X-SOA-Total-Pages': total_pages}
 
 
-    @ns.expect(user)
-    @ns.marshal_with(user, code=201)
+    @ns.expect(user_ext)
+    @ns.marshal_with(user_ext, code=201)
     @ns.response(400, 'There is a problem with your request data')
     def post(self):
         ''' To create a user. Key \'disabled\' is ignored as this is automatically set to false at creation '''
@@ -105,7 +116,7 @@ class UserList(Resource):
 class User(Resource):
     user_not_found = 'User not found'
 
-    @ns.marshal_with(user)
+    @ns.marshal_with(user_ext)
     def get(self, id):
         ''' To fetch a user '''
         try:
@@ -120,8 +131,8 @@ class User(Resource):
         return usr
 
 
-    @ns.expect(user)
-    @ns.marshal_with(user)
+    @ns.expect(user_ext)
+    @ns.marshal_with(user_ext)
     def put(self, id):
         ''' To update a user '''
         try:
@@ -138,7 +149,7 @@ class User(Resource):
         return usr
 
 
-    @ns.marshal_with(user)
+    @ns.marshal_with(user_ext)
     def delete(self, id):
         ''' To delete a user '''
         try:
@@ -162,7 +173,7 @@ class Catalog(Resource):
     def get(self):
         ''' To fetch an object containing data for screen fields (key: table name, value: list of table rows) '''
         try:
-            field_catalog = users.get_catalogs(['divisions', 'orgchart_roles'])
+            field_catalog = users.get_catalogs(['divisions', 'orgchart_roles', 'authorities'])
         except psycopg2.Error as err:
             ns.abort(500, message=get_msg_pgerror(err))
         except Exception as err:

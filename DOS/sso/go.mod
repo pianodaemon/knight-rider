b1a359105b2e86ed576ae357b9138e47915d109b
Bob Ross <pianodaemon@gmail.com>
2020-04-26 09:58:25 -0500

    sso: http server basic skeleton with logging
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

---
+++ DOS/sso/go.mod
@@ -0,0 +1,11 @@
+module immortalcrab.com/sso
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/meatballhat/negroni-logrus v1.1.0
+	github.com/sirupsen/logrus v1.4.2
+	github.com/urfave/negroni v1.0.0
+)
---
+++ DOS/sso/pkg/service/glue.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/sirupsen/logrus"
+
+	"immortalcrab.com/sso/internal/rsapi"
+)
+
+// Engages the RESTful API
+func Engage(logger *logrus.Logger) error {
+
+	var apiSettings rsapi.RestAPISettings
+	envconfig.Process("rsapi", &apiSettings)
+
+	/* The connection of both components occurs through
+	   the router glue and its adaptive functions */
+	glue := func(api *rsapi.RestAPI) *mux.Router {
+
+		router := mux.NewRouter()
+
+		v1 := router.PathPrefix("/v1").Subrouter()
+
+		mgmt := v1.PathPrefix("/sso").Subrouter()
+
+		mgmt.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		})
+		return router
+	}
+
+	api := rsapi.NewRestAPI(logger, &apiSettings, glue)
+
+	api.PowerOn()
+
+	return nil
+}

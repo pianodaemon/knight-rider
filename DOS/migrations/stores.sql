f9ca05c56736e32cb3abce29c16eac36bb0ea0b6
Miriam <miriam.physics@gmail.com>
2020-05-02 17:31:17 -0500

    Store procedure por results_report

--- DOS/migrations/stores.sql
+++ DOS/migrations/stores.sql
@@ -390,3 +390,260 @@ $$;
 ALTER FUNCTION public.alter_user(_user_id integer, _username character varying, _passwd character varying, _orgchart_role_id integer, _division_id integer, _access_vector integer[], _disabled boolean) OWNER TO postgres;
 
 
+
+
+
+
+
+--
+-- Name: alter_result_report(integer, integer, integer, integer, integer, integer, integer, text, double precision, double precision, double precision, text, date, date, date, date, date, text, text, text, text, text, integer, text, date, date, date); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.alter_result_report(
+        _results_report_id integer,
+        _type_id integer, 
+        _code_id integer, 
+        _bis_code_id integer, 
+        _social_program_id integer, 
+        _audit_id integer, 
+        _fiscal_id integer, 
+        _observation_title text, 
+        _amount_observed double precision, 
+        _amount_projected double precision, 
+        _amount_solved double precision, 
+        _amount_comments text, 
+        _reception_date date, 
+        _expiration_date date, 
+        _doc_a_date date, 
+        _doc_b_date date, 
+        _doc_c_date date, 
+        _doc_a text, 
+        _doc_b text, 
+        _doc_c text, 
+        _dep_response text, 
+        _dep_resp_comments text, 
+        _division_id integer, 
+        _hdr_doc text, 
+        _hdr_reception_date date, 
+        _hdr_expiration1_date date, 
+        _hdr_expiration2_date date
+    ) RETURNS record
+    LANGUAGE plpgsql
+    AS $$
+
+   
+DECLARE
+
+    -- latter amount to be compared with the newer one
+    latter_amount amounts_result_report;
+
+    current_moment timestamp with time zone = now();
+    coincidences integer := 0;
+    latter_id integer := 0;
+	
+	-- get the default observation stage's id value (new observations only):
+	default_obs_stage_id integer := 0;
+	default_obs_stage_title text := 'PRELIMINAR';
+	prelim_counter integer := 0;
+
+    -- dump of errors
+    rmsg text;
+
+BEGIN
+
+    CASE
+
+        WHEN _results_report_id = 0 THEN
+
+            SELECT count(id)
+			FROM observation_stages INTO prelim_counter
+			WHERE title = default_obs_stage_title;
+
+			IF NOT prelim_counter = 1 THEN
+				RAISE EXCEPTION 'Value % does not exist', default_obs_stage_title;
+			END IF;
+
+			SELECT id
+			FROM observation_stages INTO default_obs_stage_id
+			WHERE title = default_obs_stage_title;
+
+			
+            INSERT INTO results_report (
+                observation_type_id,
+                observation_code_id,
+                observation_bis_code_id,
+                social_program_id,
+                audit_id,
+                observation_title,
+                fiscal_id,
+                amount_observed,
+                reception_date,
+                expiration_date,
+                doc_a_date,
+                doc_b_date,
+                doc_c_date,
+                doc_a,
+                doc_b,
+                doc_c,
+                dep_response,
+                dep_resp_comments,
+                division_id,
+                hdr_doc,
+                hdr_reception_date,
+                hdr_expiration1_date,
+                hdr_expiration2_date,
+                inception_time,
+                touch_latter_time,
+				observation_stage_id
+            ) VALUES (
+                _type_id,
+                _code_id,
+                _bis_code_id,
+                _social_program_id,
+                _audit_id,
+                _observation_title,
+                _fiscal_id,
+                _amount_observed,
+                _reception_date,
+                _expiration_date,
+                _doc_a_date,
+                _doc_b_date,
+                _doc_c_date,
+                _doc_a,
+                _doc_b,
+                _doc_c,
+                _dep_response,
+                _dep_resp_comments,
+                _division_id,
+                _hdr_doc,
+                _hdr_reception_date,
+                _hdr_expiration1_date,
+                _hdr_expiration2_date,
+                current_moment,
+                current_moment,
+				default_obs_stage_id
+            ) RETURNING id INTO latter_id;
+
+            INSERT INTO amounts_result_report (
+                comments,
+                projected,
+                solved,
+                result_report_id,
+                inception_time
+            ) VALUES (
+                _amount_comments,
+                _amount_projected,
+                _amount_solved,
+                latter_id,
+                current_moment
+            );
+            
+
+        WHEN _results_report_id > 0 THEN
+
+            -- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            -- STARTS - Validates result_report id
+            --
+            -- JUSTIFICATION: Because UPDATE statement does not issue
+            -- any exception if nothing was updated.
+            -- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            SELECT count(id)
+            FROM results_report INTO coincidences
+            WHERE NOT blocked AND id = _results_report_id;
+
+            IF NOT coincidences = 1 THEN
+                RAISE EXCEPTION 'result report identifier % does not exist', _results_report_id;
+            END IF;
+            -- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            -- ENDS - Validate observation id
+            -- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+            UPDATE results_report
+            SET observation_title  = _observation_title, observation_type_id = _type_id, observation_code_id = _code_id,
+                observation_bis_code_id = _bis_code_id, social_program_id = _social_program_id,
+                audit_id = _audit_id, fiscal_id = _fiscal_id, reception_date = _reception_date,
+                expiration_date = _expiration_date, doc_a_date = _doc_a_date,
+                doc_b_date = _doc_b_date, doc_c_date = _doc_c_date, doc_a = _doc_a, doc_b = _doc_b,
+                doc_c = _doc_c, dep_response = _dep_response, dep_resp_comments = _dep_resp_comments,
+                division_id = _division_id, hdr_doc = _hdr_doc, hdr_reception_date = _hdr_reception_date,
+                hdr_expiration1_date = _hdr_expiration1_date, hdr_expiration2_date = _hdr_expiration2_date,
+                amount_observed = _amount_observed, touch_latter_time = current_moment
+            WHERE id = _results_report_id;
+
+            SELECT * FROM amounts_result_report
+            WHERE amounts_result_report.result_report_id = _results_report_id
+            ORDER BY inception_time DESC LIMIT 1
+            INTO latter_amount;
+
+            -- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            -- Insertion solely occurs if any of solved or projected fields
+            -- contains newer data otherwise nothing regarding amounts
+            -- shall be inserted as the latest version of such fields.
+            -- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            IF latter_amount.solved != _amount_solved OR latter_amount.projected != _amount_projected THEN
+
+                INSERT INTO amounts_result_report (
+                    comments,
+                    projected,
+                    solved,
+                    result_report_id,
+                    inception_time
+                ) VALUES (
+                    _amount_comments,
+                    _amount_projected,
+                    _amount_solved,
+                    _results_report_id,
+                    current_moment
+                );
+
+            END IF;
+
+            -- Upon edition we return observation id as latter id
+            latter_id = _results_report_id;
+
+        ELSE
+            RAISE EXCEPTION 'negative result resport identifier % is unsupported', result_report_id;
+
+    END CASE;
+
+    RETURN ( latter_id::integer, ''::text );
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            GET STACKED DIAGNOSTICS rmsg = MESSAGE_TEXT;
+            return ( -1::integer, rmsg::text );
+
+END;
+$$;
+
+
+ALTER FUNCTION public.alter_result_report(
+    _results_report_id integer, 
+    _type_id integer, 
+    _code_id integer, 
+    _bis_code_id integer, 
+    _social_program_id integer, 
+    _audit_id integer, 
+    _fiscal_id integer, 
+    _observation_title text, 
+    _amount_observed double precision, 
+    _amount_projected double precision, 
+    _amount_solved double precision, 
+    _amount_comments text, 
+    _reception_date date, 
+    _expiration_date date, 
+    _doc_a_date date, 
+    _doc_b_date date, 
+    _doc_c_date date, 
+    _doc_a text, 
+    _doc_b text, 
+    _doc_c text, 
+    _dep_response text, 
+    _dep_resp_comments text, 
+    _division_id integer, 
+    _hdr_doc text, 
+    _hdr_reception_date date, 
+    _hdr_expiration1_date date, 
+    _hdr_expiration2_date date) OWNER TO postgres;
+
+

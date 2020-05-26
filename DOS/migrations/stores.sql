a79d8cefc92d7719052b96dc0e27ea2019d49df7
userd <omontes.dev@gmail.com>
2020-05-26 10:58:36 -0500

    Observacions SFP: Database schema changes + Endpoint

--- DOS/migrations/stores.sql
+++ DOS/migrations/stores.sql
@@ -82,6 +82,185 @@ ALTER FUNCTION public.alter_audit(_audit_id integer, _title character varying, _
 
 
 --
+-- Name: alter_observacion_sfp(integer, integer[], integer, integer, date, integer, integer, character varying, date, date, integer, text, character varying, character varying, integer, double precision, public.seguimientos_obs_sfp[], double precision, double precision, date, double precision, character varying, date, character varying, date, character varying, character varying, date, integer, character varying, date, character varying, character varying, date); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.alter_observacion_sfp(_observacion_id integer, _anios_cta_publica integer[], _direccion_id integer, _dependencia_id integer, _fecha_captura date, _programa_social_id integer, _auditoria_id integer, _acta_cierre character varying, _fecha_firma_acta_cierre date, _fecha_compromiso date, _clave_observacion_id integer, _observacion text, _acciones_correctivas character varying, _acciones_preventivas character varying, _tipo_observacion_id integer, _monto_observado double precision, _seguimientos public.seguimientos_obs_sfp[], _monto_a_reintegrar double precision, _monto_reintegrado double precision, _fecha_reintegro date, _monto_por_reintegrar double precision, _num_oficio_of_vista_cytg character varying, _fecha_oficio_of_vista_cytg date, _num_oficio_cytg_aut_invest character varying, _fecha_oficio_cytg_aut_invest date, _num_carpeta_investigacion character varying, _num_oficio_vai_municipio character varying, _fecha_oficio_vai_municipio date, _autoridad_invest_id integer, _num_oficio_pras_of character varying, _fecha_oficio_pras_of date, _num_oficio_pras_cytg_dependencia character varying, _num_oficio_resp_dependencia character varying, _fecha_oficio_resp_dependencia date) RETURNS record
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+
+    -- latter amount to be compared with the newer one
+    --latter_amount amounts;
+
+    current_moment timestamp with time zone = now();
+    --coincidences integer := 0;
+    ult_obs_id integer := 0;
+	seguimientos_arr_len integer := array_length(_seguimientos, 1);
+	anios_cta_publica_arr_len integer := array_length(_anios_cta_publica, 1);
+	anios_aux_arr integer[];
+	idx integer := 0;
+	
+	-- get the default observation stage's id value (new observations only):
+	--default_obs_stage_id integer := 0;
+	--default_obs_stage_title text := 'PRELIMINAR';
+	--prelim_counter integer := 0;
+
+    -- dump of errors
+    rmsg text;
+
+BEGIN
+
+    CASE
+
+        WHEN _observacion_id = 0 THEN
+		
+            INSERT INTO observaciones_sfp (
+                direccion_id,
+				dependencia_id,
+				fecha_captura,
+				programa_social_id,
+				auditoria_id,
+				acta_cierre,
+				fecha_firma_acta_cierre,
+				fecha_compromiso,
+				clave_observacion_id,
+				observacion,
+				acciones_correctivas,
+				acciones_preventivas,
+				tipo_observacion_id,
+				monto_observado,
+				monto_a_reintegrar,
+				monto_reintegrado,
+				fecha_reintegro,
+				monto_por_reintegrar,
+				num_oficio_of_vista_cytg,
+				fecha_oficio_of_vista_cytg,
+				num_oficio_cytg_aut_invest,
+				fecha_oficio_cytg_aut_invest,
+				num_carpeta_investigacion,
+				num_oficio_vai_municipio,
+				fecha_oficio_vai_municipio,
+				autoridad_invest_id,
+				num_oficio_pras_of,
+				fecha_oficio_pras_of,
+				num_oficio_pras_cytg_dependencia,
+				num_oficio_resp_dependencia,
+				fecha_oficio_resp_dependencia,
+				hora_ult_cambio,
+				hora_creacion
+            ) VALUES (
+                _direccion_id,
+				_dependencia_id,
+				_fecha_captura,
+				_programa_social_id,
+				_auditoria_id,
+				_acta_cierre,
+				_fecha_firma_acta_cierre,
+				_fecha_compromiso,
+				_clave_observacion_id,
+				_observacion,
+				_acciones_correctivas,
+				_acciones_preventivas,
+				_tipo_observacion_id,
+				_monto_observado,
+				_monto_a_reintegrar,
+				_monto_reintegrado,
+				_fecha_reintegro,
+				_monto_por_reintegrar,
+				_num_oficio_of_vista_cytg,
+				_fecha_oficio_of_vista_cytg,
+				_num_oficio_cytg_aut_invest,
+				_fecha_oficio_cytg_aut_invest,
+				_num_carpeta_investigacion,
+				_num_oficio_vai_municipio,
+				_fecha_oficio_vai_municipio,
+				_autoridad_invest_id,
+				_num_oficio_pras_of,
+				_fecha_oficio_pras_of,
+				_num_oficio_pras_cytg_dependencia,
+				_num_oficio_resp_dependencia,
+				_fecha_oficio_resp_dependencia,
+				current_moment,
+				current_moment
+            ) RETURNING id INTO ult_obs_id;
+			
+            FOR idx IN 1 .. seguimientos_arr_len LOOP
+
+                INSERT INTO seguimientos_obs_sfp(
+                    observacion_id,
+					seguimiento_id,
+					num_oficio_cytg_oic,
+					fecha_oficio_cytg_oic,
+					fecha_recibido_dependencia,
+					fecha_vencimiento_cytg,
+					num_oficio_resp_dependencia,
+					fecha_recibido_oficio_resp,
+					resp_dependencia,
+					comentarios,
+					clasif_final_interna_cytg,
+					num_oficio_org_fiscalizador,
+					fecha_oficio_org_fiscalizador,
+					estatus_id,
+					monto_solventado,
+					monto_pendiente_solventar
+                ) VALUES (
+                    ult_obs_id,
+					_seguimientos[idx].seguimiento_id,
+					_seguimientos[idx].num_oficio_cytg_oic,
+					_seguimientos[idx].fecha_oficio_cytg_oic,
+					_seguimientos[idx].num_oficio_cytg_oic,
+					_seguimientos[idx].fecha_oficio_cytg_oic,
+					_seguimientos[idx].fecha_recibido_dependencia,
+					_seguimientos[idx].fecha_vencimiento_cytg,
+					_seguimientos[idx].num_oficio_resp_dependencia,
+					_seguimientos[idx].fecha_recibido_oficio_resp,
+					_seguimientos[idx].resp_dependencia,
+					_seguimientos[idx].comentarios,
+					_seguimientos[idx].clasif_final_interna_cytg,
+					_seguimientos[idx].num_oficio_org_fiscalizador,
+					_seguimientos[idx].fecha_oficio_org_fiscalizador,
+					_seguimientos[idx].estatus_id,
+					_seguimientos[idx].monto_solventado,
+					_seguimientos[idx].monto_pendiente_solventar
+                );
+            
+			END LOOP;
+			
+			for idx in 1 .. anios_cta_publica_arr_len loop
+				anios_aux_arr[idx] = ult_obs_id;
+			end loop;
+			
+			INSERT INTO anios_cuenta_publica_obs_sfp
+			select *
+			from unnest(anios_aux_arr, _anios_cta_publica);
+			
+
+        WHEN _observacion_id > 0 THEN
+
+            
+
+        ELSE
+            RAISE EXCEPTION 'negative observation identifier % is unsupported', _observacion_id;
+
+    END CASE;
+
+    RETURN ( ult_obs_id::integer, ''::text );
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            GET STACKED DIAGNOSTICS rmsg = MESSAGE_TEXT;
+            return ( -1::integer, rmsg::text );
+
+END;
+$$;
+
+
+ALTER FUNCTION public.alter_observacion_sfp(_observacion_id integer, _anios_cta_publica integer[], _direccion_id integer, _dependencia_id integer, _fecha_captura date, _programa_social_id integer, _auditoria_id integer, _acta_cierre character varying, _fecha_firma_acta_cierre date, _fecha_compromiso date, _clave_observacion_id integer, _observacion text, _acciones_correctivas character varying, _acciones_preventivas character varying, _tipo_observacion_id integer, _monto_observado double precision, _seguimientos public.seguimientos_obs_sfp[], _monto_a_reintegrar double precision, _monto_reintegrado double precision, _fecha_reintegro date, _monto_por_reintegrar double precision, _num_oficio_of_vista_cytg character varying, _fecha_oficio_of_vista_cytg date, _num_oficio_cytg_aut_invest character varying, _fecha_oficio_cytg_aut_invest date, _num_carpeta_investigacion character varying, _num_oficio_vai_municipio character varying, _fecha_oficio_vai_municipio date, _autoridad_invest_id integer, _num_oficio_pras_of character varying, _fecha_oficio_pras_of date, _num_oficio_pras_cytg_dependencia character varying, _num_oficio_resp_dependencia character varying, _fecha_oficio_resp_dependencia date) OWNER TO postgres;
+
+
+--
 -- Name: alter_observation_preliminar(integer, integer, integer, integer, integer, integer, integer, text, double precision, double precision, double precision, text, date, date, date, date, date, text, text, text, text, text, integer, text, date, date, date); Type: FUNCTION; Schema: public; Owner: postgres
 --
 

b3be225be9587dfedab54554ebeb7d3fb89e7c8c
userd <omontes.dev@gmail.com>
2020-08-19 01:38:27 -0500

    User Endpoints security: Verify token

--- DOS/soa/service/misc/helper.py
+++ DOS/soa/service/misc/helper.py
@@ -1,3 +1,6 @@
+import jwt
+import time
+
 def get_search_params(args, fields):
     params = {}
     for key in fields:
@@ -8,3 +11,25 @@ def get_search_params(args, fields):
         return None
 
     return params
+
+
+def verify_token(headers):
+    if 'Authorization' not in headers:
+        raise Exception('No se encontró un token en el request')
+    
+    auth = headers['Authorization']
+    auth = auth.replace('Bearer ', '')
+
+    with open('../pem/public_key.pub', 'rb') as f:
+        public_key = f.read()
+
+    try:
+        decoded = jwt.decode(auth, public_key, algorithms='RS512')
+    except:
+        raise Exception('El token no es válido')
+    
+    curr_time = int(time.time())
+    exp_time = decoded['exp']
+
+    if curr_time > exp_time:
+        raise Exception('El token ha expirado')

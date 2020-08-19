b47cbc74a4aa2a4f97d678a14c671de1bdee054a
userd <omontes.dev@gmail.com>
2020-08-19 14:28:21 -0500

    User Endpoints security: removed redundant public key file and improved when it is read from filesystem

--- DOS/soa/service/genl/restplus.py
+++ DOS/soa/service/genl/restplus.py
@@ -3,3 +3,9 @@ from custom.profile import MSERV_NAME, MSERV_DESC
 
 
 api = Api(version='1.0', title=MSERV_NAME, description=MSERV_DESC)
+
+try:
+    with open('../pem/public_key.pub', 'rb') as f:
+        public_key = f.read()
+except:
+    public_key = None

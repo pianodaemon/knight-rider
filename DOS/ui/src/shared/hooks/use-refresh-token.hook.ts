e83492d8907359cf6940677d19b9b36142915a15
Héctor Cerón Figueroa <linuxeron@gmail.com>
2020-08-24 20:14:43 -0500

    Fixing useRefreshToken hook. (#186)
    * Several Minor changes on Observations Form and Lists
    
    * Added AutoCompleteDropdown Component.
    
    * Added Observations SFP module. WIP CRUD
    
    * Obs_SFP WIP
    
    * Observaciones SFP CRUD WIP
    
    * Obs SFP Table - Truncate 5 lines to Observación attr.
    
    * Hotfix Obs SFP (Removed old hardcodes).
    
    * Removed Duplicated Module files Obs SFP
    
    * Obs_SFP CRUD completed. @Action item: Validaciones Seguimientos.
    
    * Pull dependencia from catalog.
    
    * Refactored Año Auditoría & dependencia selectors
    
    * Fixed dependencia_id from catalog.
    
    * Obs ASF (WIP)
    
    * Obs Pre ASF (WIP)
    
    * Added Informe de Resultados List and Form
    
    * Added Single Text Modal component. Field fixes on Obs_SFP.
    
    * Obs ASF CRUD completed.
    
    * Audit Years and Dependencies updated. Bunch of changes on SFP, ASF and IR forms.
    
    * UI feedback fixes.
    
    * SFP & ASF user feedback changes.
    
    * Fix Proyecciones catalog
    
    * Added calculated field Monto por Reintegrar
    
    * AutoCompleteTreeDropdown component added. Modified Audits form.
    
    * Modified ASF form num_observacion field.
    
    * Fixed fecha_captura default value on SFP form
    
    * Fixed fecha_captura default value on ASF form
    
    * Feedback comments SFP. IRes changes: WIP.
    
    * Fixed substract issue on SFP form.
    
    * SFP feedback changes. IRes items WIP.
    
    * Added clasifs_internas_cytg catalog to ASF.
    
    * Added clasif_final_interna_cytg catalog options for IRes Form.
    
    * Changes on IRes Form. Several items still WIP.
    
    * Load More options component for IRes Form.
    
    * Added First and Last name fields on User Form
    
    * IRes changes.
    
    * Obs ASENL Form/List.
    
    * IRes ASENL
    
    * Replaced Programa to Dependencias on Obs List ASE
    
    * Feedback changes and fixes.
    
    * Last feedback changes.
    
    * More feedback changes. Catalog Abbreviations.
    
    * View mode text color for Obs ASENL
    
    * Selectors fix.
    
    * Observaciones Preliminares CyTG CRUD
    
    * Removed Crear option menu
    
    * Obs IRes CyTG CRUD.
    
    * Added SSO keys and moved UI dir to DOS.
    
    * SSO and login (WIP)
    
    * Feedback Changes
    
    * Collapse Menu.
    
    * Logout feature (WIP).
    
    * JWT auth for User CRUD.
    
    * Fixing useRefreshToken hook.

--- DOS/ui/src/shared/hooks/use-refresh-token.hook.ts
+++ DOS/ui/src/shared/hooks/use-refresh-token.hook.ts
@@ -1,13 +1,15 @@
 import { useRef, useEffect } from 'react';
-import { TokenStorage } from '../utils/token-storage.util';
+// import { TokenStorage } from '../utils/token-storage.util';
 
 export const useRefreshToken = (fn: Function) => {
   const cb = useRef(fn);
   cb.current = fn;
 
+  /*
   if (TokenStorage.isAuthenticated()) {
     const { exp } = TokenStorage.getTokenClaims();
   }
+  */
   
   useEffect(() => {
     const onUnload = (e: any) => cb.current(e);

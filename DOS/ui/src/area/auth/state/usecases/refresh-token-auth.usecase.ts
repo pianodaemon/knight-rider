12b36a25b57ce3e6fc67500265a023aee336eece
telematica <linuxeron@gmail.com>
2020-08-25 19:19:18 -0500

    Refresh token feature completed.

--- DOS/ui/src/area/auth/state/usecases/refresh-token-auth.usecase.ts
+++ DOS/ui/src/area/auth/state/usecases/refresh-token-auth.usecase.ts
@@ -1,12 +1,11 @@
 import { Action, createAction, ActionFunctionAny } from 'redux-actions';
 import { call, put, takeLatest } from 'redux-saga/effects';
-import Cookies from 'universal-cookie';
-import jwt_decode from 'jwt-decode';
 import { mergeSaga } from 'src/redux-utils/merge-saga';
 import { notificationAction } from 'src/area/main/state/usecase/notification.usecase';
 import { translations } from 'src/shared/translations/translations.util';
 import { refreshToken } from '../../service/auth.service';
 import { authReducer } from '../auth.reducer';
+import { TokenStorage } from 'src/shared/utils/token-storage.util';
 
 const postfix = '/app';
 const REFRESH_TOKEN_AUTH = `REFRESH_TOKEN_AUTH${postfix}`;
@@ -23,32 +22,29 @@ export const refreshTokenAuthErrorAction: ActionFunctionAny<
   Action<any>
 > = createAction(REFRESH_TOKEN_AUTH_ERROR);
 
-function* refreshTokenAuthWorker(action: any): Generator<any, any, any> {
+function* refreshTokenAuthWorker(): Generator<any, any, any> {
   try {
-    const { credentials, history } = action.payload;
-    const result = yield call(refreshToken, '');
+    const { sub: userId } = TokenStorage.getTokenClaims() || {};
+    const result = yield call(refreshToken, userId);
     const { token } = result.data;
-    yield setAuthCookie(token);
+    TokenStorage.storeToken(token);
     yield put(refreshTokenAuthSuccessAction(token));
-    yield history.push('/audit/list');
     yield put(
       notificationAction({
-        message: `¡Bienvenido!`,
+        message: `¡Su sesión ha sido renovada!`,
         type: 'success',
       })
     );
   } catch (e) {
-    const { releaseForm } = action.payload;
     let message: string =
       e.response && e.response.data && e.response.data.message
         ? e.response.data.message
-        : '¡Error de inesperado! Por favor contacte al Administrador.';
+        : '¡Error al renovar su sesión! Por favor contacte al Administrador.';
     message = message.includes(
       translations.observation.error_responses.unique_constraint
     )
       ? translations.observation.error_responses.unique_error
       : message;
-    yield releaseForm();
     yield put(refreshTokenAuthErrorAction(e));
     yield put(
       notificationAction({
@@ -64,34 +60,20 @@ function* refreshTokenAuthWatcher(): Generator<any, any, any> {
   yield takeLatest(REFRESH_TOKEN_AUTH, refreshTokenAuthWorker);
 }
 
-function setAuthCookie(token: string): void {
-  const cookies = new Cookies();
-  const jwt: any = jwt_decode(token);
-  cookies.set(
-    'token',
-    token,
-    { 
-      path: '/',
-      expires: new Date(jwt.exp * 1000)
-    }
-  );
-}
-
 const authReducerHandlers = {
   [REFRESH_TOKEN_AUTH]: (state: any) => {
     return {
       ...state,
       loading: true,
-      // token: null,
-      signedIn: false,
+      refreshing: true,
     };
   },
   [REFRESH_TOKEN_AUTH_SUCCESS]: (state: any, action: any) => {
-    // const { token } = action.payload;
     return {
       ...state,
       loading: false,
       signedIn: true,
+      refreshing: false,
     };
   },
   [REFRESH_TOKEN_AUTH_ERROR]: (state: any, action: any) => {
@@ -100,6 +82,7 @@ const authReducerHandlers = {
       error: action.payload,
       loading: false,
       signedIn: false,
+      refreshing: false
     };
   },
 };
---
+++ DOS/ui/src/shared/constants/time.constants.ts
@@ -0,0 +1,2 @@
+export const REFRESH_THRESHOLD_MS = 300000; // 5 minutes 
+export const REFRESH_TIMEOUT_MS = 0; // 0 milliseconds

e128d907e2d13439900a88605cdb04179365d8ad
ombdev <omontes.dev@gmail.com>
2020-10-24 21:52:56 -0500

    Corner case: handle error hit when a preliminar id referenced by an IRes obs has been deleted (#226)
    * Needed to keep users endpoints working in master branch
    
    * SSO token security: Added validation against black list (logged out)
    
    * SOA nginx config: La incondicional
    
    * SOA nginx config: minor fix
    
    * SSO token security: Added claim with user authorities
    
    * SSO token security: Claim authorities improved
    
    * SOA files endpoints: Down/Uploading
    
    * SOA file services: Swagger doc improved
    
    * Endpoints de listado de SFP, ASF pre, ASENL pre, CyTG pre: Filtrado por direccion_id del user (#207)
    
    * Pre Obs division filter. User form modified.
    
    * Pre Obs division filter. User form modified. (#208)
    
    * Added Clave de Observación on Obs Tables. Modified SFP form date labels.
    
    * Endpoints de listado de SFP, ASF pre, ASENL pre, CyTG pre: Filtrado por direccion_id del user
    
    * Pre Obs division filter. User form modified. (#208)
    
    * Vergueletor: reapply commits on top of master branch (#211)
    
    * Needed to keep users endpoints working in master branch
    
    * SSO token security: Added validation against black list (logged out)
    
    * SOA nginx config: La incondicional
    
    * SOA nginx config: minor fix
    
    * SSO token security: Added claim with user authorities
    
    * SSO token security: Claim authorities improved
    
    * SOA files endpoints: Down/Uploading
    
    * SOA file services: Swagger doc improved
    
    * Se incluyo el filtro de la direccion del usuario para las observaciones de los reportes (#209)
    
    * Se agrego el filtro por division del usuario a los endpoints
    
    * Se incluyo el id_usuario en el querystring de los componentes react
    
    * Endpoints de listado de SFP, ASF pre, ASENL pre, CyTG pre: Filtrado por direccion_id del user
    
    * Pre Obs division filter. User form modified. (#208)
    
    Co-authored-by: Miriam R <neurona13@gmail.com>
    Co-authored-by: Héctor Cerón Figueroa <linuxeron@gmail.com>
    
    * Catalog endpoints: SFP and all Pre catalogs now allow direccion_id filtering
    
    * Corner case: handle error when a preliminar id referenced by an IRes obs has been deleted
    
    Co-authored-by: telematica <linuxeron@gmail.com>
    Co-authored-by: Miriam R <neurona13@gmail.com>
    Co-authored-by: Bob Ross <pianodaemon@gmail.com>

--- DOS/soa/service/dal/observaciones_ires_cytg.py
+++ DOS/soa/service/dal/observaciones_ires_cytg.py
@@ -338,7 +338,7 @@ def seguimientos_to_comp_type_arr_lit(seguimientos):
 
 
 def add_preliminar_data(ent):
-    if ent['observacion_pre_id'] < 1:
+    if ent['observacion_pre_id'] <= 0:
         ent['direccion_id'] = None
         ent['programa_social_id'] = None
         ent['auditoria_id'] = None
@@ -350,13 +350,16 @@ def add_preliminar_data(ent):
             AND NOT blocked;
         '''.format(ent['observacion_pre_id'])
 
-        rows = exec_steady(sql)
-
-        row = dict(rows[0])
-        ent['direccion_id'] = row['direccion_id']
-        ent['programa_social_id'] = row['programa_social_id']
-        ent['auditoria_id'] = row['auditoria_id']
-
+        try:
+            rows = exec_steady(sql)
+            row = dict(rows[0])
+            ent['direccion_id'] = row['direccion_id']
+            ent['programa_social_id'] = row['programa_social_id']
+            ent['auditoria_id'] = row['auditoria_id']
+        except Exception as err:
+            ent['direccion_id'] = None
+            ent['programa_social_id'] = None
+            ent['auditoria_id'] = None
 
 def transform_list_into_dict(input_list):
     output_dict = {}

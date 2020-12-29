c40eb6aec8e53e15541164665040ff49b93b4f86
ombdev <omontes.dev@gmail.com>
2020-12-28 21:45:53 -0600

    Dependencias: endpoints created (#282)
    * Needed to keep users endpoints working in master branch
    
    * SSO token security: Added validation against black list (logged out)
    
    * SOA nginx config: La incondicional
    
    * SOA nginx config: minor fix
    
    * SSO token security: Added claim with user authorities
    
    * SSO token security: Claim authorities improved
    
    * SOA files endpoints: Down/Uploading
    
    * SOA file services: Swagger doc improved
    
    * Endpoints de listado de SFP, ASF pre, ASENL pre, CyTG pre: Filtrado por direccion_id del user (#207)
    
    * Pre Obs division filter. User form modified.
    
    * Pre Obs division filter. User form modified. (#208)
    
    * Added Clave de Observación on Obs Tables. Modified SFP form date labels.
    
    * Endpoints de listado de SFP, ASF pre, ASENL pre, CyTG pre: Filtrado por direccion_id del user
    
    * Pre Obs division filter. User form modified. (#208)
    
    * Vergueletor: reapply commits on top of master branch (#211)
    
    * Needed to keep users endpoints working in master branch
    
    * SSO token security: Added validation against black list (logged out)
    
    * SOA nginx config: La incondicional
    
    * SOA nginx config: minor fix
    
    * SSO token security: Added claim with user authorities
    
    * SSO token security: Claim authorities improved
    
    * SOA files endpoints: Down/Uploading
    
    * SOA file services: Swagger doc improved
    
    * Se incluyo el filtro de la direccion del usuario para las observaciones de los reportes (#209)
    
    * Se agrego el filtro por division del usuario a los endpoints
    
    * Se incluyo el id_usuario en el querystring de los componentes react
    
    * Endpoints de listado de SFP, ASF pre, ASENL pre, CyTG pre: Filtrado por direccion_id del user
    
    * Pre Obs division filter. User form modified. (#208)
    
    Co-authored-by: Miriam R <neurona13@gmail.com>
    Co-authored-by: Héctor Cerón Figueroa <linuxeron@gmail.com>
    
    * Catalog endpoints: SFP and all Pre catalogs now allow direccion_id filtering
    
    * Corner case: handle error when a preliminar id referenced by an IRes obs has been deleted
    
    * Audits endpoints: Added direccion_id and org_fiscal_id
    
    * Filtrado por direccion_id agregado a listado de Audits. Campos org_fiscal_id y direccion_id agregados a catalog endpoints de los 7 forms de orgs fiscal
    
    * Listo el Num de Obs para listados de IRes de ASF, ASENL y CyTG. Agregados 4 campos de reintegros a nivel seguimiento de SFP y ASF
    
    * Add clave_observacion y num_observacion a search params
    
    * Pre ASF, ASENL y CyTG: Added search params
    
    * Pre CyTG: Campo estatus_id agregado
    
    * IRes ASENL endpoints: Added tipificacion_id
    
    * ASF IRes: Changed to integer the type of field accion. Added a values catalog to it
    
    * SFP: Escaping single quote character
    
    * Campos reintegros a seguimientos de IRes CyTG. Filtrado por direccion_id, auditoria_id, num_observacion para IRes ASF, ASENL, CyTG. Escape de single quote character
    
    * Fix de filtro por num_observacion para IRes ASENL y CyTG (no debe buscarse en preliminares)
    
    * Se agregaron filtros por dependencia_id and anio_cuenta_pub en listados de SFP y Preliminares de ASF, ASENL, CyTG
    
    * Se agregaron filtros por dependencia_id and anio_cuenta_pub en listados de IRes de ASF, ASENL, CyTG
    
    * SFP: Fixed record counting for listing endpoint, now includes search params from other tables
    
    * Pre ASF, ASENL, CyTG: Fixed record counting for listing endpoint, now includes search params from other tables
    
    * Fixed both filters (dependencia_id and anio_cuenta_pub) intended for all orgs. fisc.
    
    * Audit listing improved: Fetching is now ~35x faster
    
    * Fixed a typo in identifier name
    
    * Param not needed, forgot to remove
    
    * Programas sociales: endpoints created
    
    * Dependencias: endpoints created
    
    Co-authored-by: telematica <linuxeron@gmail.com>
    Co-authored-by: Miriam R <neurona13@gmail.com>
    Co-authored-by: Bob Ross <pianodaemon@gmail.com>

---
+++ DOS/soa/service/dal/dependencias.py
@@ -0,0 +1,131 @@
+import math
+
+from dal.helper import exec_steady
+from dal.entity import page_entities, count_entities
+
+def create(**kwargs):
+    ''' Crear una entidad Dependencia '''
+    sql = """
+        INSERT INTO dependencies
+        VALUES (DEFAULT, '{}', '{}', {})
+        RETURNING *
+    """.format(
+        kwargs['title'].replace("'", "''"),
+        kwargs['description'].replace("'", "''"),
+        kwargs['clasif_id']
+    )
+
+    rows = exec_steady(sql)
+    return dict(rows[0])
+
+
+def read(id):
+    ''' Recuperar una entidad Dependencia '''
+    sql = """
+        SELECT *
+        FROM dependencies
+        WHERE id = {}
+    """.format(id)
+
+    rows = exec_steady(sql)
+    return dict(rows[0])
+
+
+def update(id, **kwargs):
+    ''' Actualizar una entidad Dependencia '''
+    sql = """
+        UPDATE dependencies
+        SET title = '{}', description = '{}', clasif_id = {}
+        WHERE id = {}
+        RETURNING *
+    """.format(
+        kwargs['title'].replace("'", "''"),
+        kwargs['description'].replace("'", "''"),
+        kwargs['clasif_id'],
+        id
+    )
+
+    rows = exec_steady(sql)
+    return dict(rows[0])
+
+
+def delete(id):
+    ''' Eliminar una entidad Dependencia '''
+    sql = """
+        DELETE FROM dependencies
+        WHERE id = {}
+        RETURNING *
+    """.format(id)
+
+    rows = exec_steady(sql)
+    return dict(rows[0])
+
+
+def read_per_page(offset, limit, order_by, order, search_params, per_page, page):
+    ''' Recuperar una página de entidades Dependencia '''
+
+    # Some validations
+    offset = int(offset)
+    if offset < 0:
+        raise Exception("Value of param 'offset' should be >= 0")
+    
+    limit = int(limit)
+    if limit < 1:
+        raise Exception("Value of param 'limit' should be >= 1")
+
+    order_by_values = (
+        'id', 'title', 'description', 'clasif_id'
+    )
+    if order_by not in order_by_values:
+        raise Exception("Value of param 'order_by' should be one of the following: " + str(order_by_values))
+    
+    order_values = ('ASC', 'DESC', 'asc', 'desc')
+    if order not in order_values:
+        raise Exception("Value of param 'order' should be one of the folowing: " + str(order_values))
+
+    per_page = int(per_page)
+    page = int(page)
+    if per_page < 1 or page < 1:
+        raise Exception("Value of params 'per_page' and 'page' should be >= 1")
+
+    # Counting total number of items and fetching target page
+    table = 'dependencies'
+    total_items = count_entities(table, search_params, False)
+    if total_items > limit:
+        total_items = limit
+    
+    total_pages = math.ceil(total_items / per_page)
+
+    whole_pages_offset = per_page * (page - 1)
+    if whole_pages_offset >= total_items:
+        return [], total_items, total_pages
+    
+    target_items = total_items - whole_pages_offset
+    if target_items > per_page:
+        target_items = per_page
+
+    entities = page_entities(table, offset + whole_pages_offset, target_items, order_by, order, search_params, False)
+
+    return (entities, total_items, total_pages)
+
+
+def get_catalogs(table_name_list):
+    ''' Fetches values and captions from a list of tables. These pairs can be used on input screens '''
+    fields_d = {}
+
+    for table in table_name_list:
+        values_l = []
+        
+        sql = '''
+            SELECT *
+            FROM {}
+            ORDER BY id;
+        '''.format(table)
+
+        rows = exec_steady(sql)
+        for row in rows:
+            values_l.append(dict(row))
+        
+        fields_d[table] = values_l
+
+    return fields_d
